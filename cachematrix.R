0ec76eb55175a92e21abfa91b195f6fa43627b26
anasorm <anasorm@gmail.com>
2014-10-25 15:25:02 +0200

    R_Programming_assignment_2
    Assignment 2 R programming: calculates inverse that is catched so that
    it does not have to recalculate it over and over

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#function that creates a special "matrix" object to be used to cache its inverse
+makeCacheMatrix <- function (x = matrix()){
+    
+    inv  <- NULL
+    set <- function(y) {
+        
+        x <<- y
+        inv <<- NULL
+    }
+    
+    get <- function () x
+    setinv <- function (inverse) inv <<- inverse
+    getinv <- function() inv 
+    
+    list (set = set, get = get, setinv = setinv,getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# calculates inverse matrix
+cacheSolve <- function (x, ...) {
+    
+    inv <- x$getinv()    
+    matx<- x$get()                 # will be used to check if matrix has changes
+    
+    
+    if(!is.null(inv)) {
+        
+        p <- sum(solve(matx)== inv)  # if inv is not null checks whether matrix has changed 
+        cnt <- sum(dim(inv))
+        
+        if(p==cnt)  {              # if matrix hasn't changed returns stored inverse
+            
+            message("getting cached inverse matrix")
+            return(inv)
+        }
+        
+    }
+    
+    data <- x$get()                # gets the matrix for which the inverse will be calculated 
+    inv <- solve(data, ...)          # calculates inverse of matrix
+    x$setinv (inv)                   # sets inverse
+    inv
 }
